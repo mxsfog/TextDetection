1db088f2da12de5b72af0158d1d4d60de8a4400b
mxsfog <mxsfog@gmail.com>
2024-04-30 09:37:46 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import Conf
 from Dataset import ImageDataset, train_collate_func, val_test_collate_func
-from decoder import ctc_decode
+from decoder import ctc_decoder
 from Model import CRNN
 
 def main():
@@ -125,3 +125,102 @@ def train(model: nn.Module,
         if batch_index % Conf.mode_config['train']["print_frequency"]:
             writer.add_scalar("Train/Loss", loss.item(), batch_index + epoch * batches + 1)
             progress.display(batch_index)
+
+
+def val(model: nn.Module,
+        dataloader: DataLoader,
+        epoch: int,
+        writer: SummaryWriter,
+        mode: str) -> [float, float]:
+
+    model.eval()
+
+    total_cor = 0
+    total_files = 0
+
+    with torch.no_grad():
+        for batch_index, (_, images, target) in enumerate(dataloader):
+            total_files += images.size(0)
+
+            images = images.to(device=Conf.device, non_blocking=True)
+
+            with amp.autocast():
+                output = model(images)
+
+            output_log_probas = F.log_softmax(output, 2)
+            _, predicted_chars = ctc_decoder(output_log_probas, Conf.chars_dict)
+
+            if ''.join(predicted_chars[0]) == target[0].lower():
+                total_cor += 1
+    acc = (total_cor / total_files) * 100
+    print(f'Accuracy: {acc::2f} %')
+
+    if mode == 'valid' or mode == 'test':
+        writer.add_scalar(f"{mode}/Acc", acc, epoch + 1)
+    else:
+        raise ValueError("Unsupported mode!")
+
+
+    return acc
+
+
+class Summary(Enum):
+    NONE = 0
+    AVERAGE = 1
+    SUM = 2
+    COUNT =3
+class AverageMeter(object):
+    def __int__(self, name, fmt=':f', summary_type=Summary.AVERAGE):
+        self.name = name
+        self.fmt = fmt,
+        self.summary_type = summary_type
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+    def summary(self):
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.2f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.2f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {count:.2f}'
+        else:
+            raise ValueError(f'Invalid type {self.summary_type}')
+
+        return fmtstr.format(**self.__dict__)
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=''):
+        self.batch_fmtstr = self.get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print(''.join(entries))
+
+    def get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches//1))
+        fmt = '{:' + str(num_digits) + "d}"
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+if __name__ == '__main__':
+    main()
